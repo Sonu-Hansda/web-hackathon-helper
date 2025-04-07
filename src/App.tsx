1482b8a26c381d5b6679effa7c64c091766d3256
Sonu-Hansda <sonukumarhansda61@gmail.com>
2025-04-07 11:17:14 +0530

    added routes with all pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
-function App() {
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import LoginPage from './features/auth/LoginPage';
+import SignUpPage from './features/auth/SignUpPage';
+import HomePage from './features/home/HomePage';
+import ProfilePage from './features/profile/ProfilePage';
+import Navbar from './shared/components/Navbar';
+
+const App = () => {
 
   return (
-    <>
-        <h1>hackathon Helper</h1>
-    </>
-  )
-}
+    <Router>
+      <Navbar />
+      <div className="min-h-screen">
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route
+            path="/home"
+            element={
+                <HomePage />
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+                <ProfilePage />
+            }
+          />
 
-export default App
+          <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
